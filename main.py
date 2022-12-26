59b1b29d9b5ac54790aed782c8d93d61abbf345b
leschi4banane <leschi.4.banane@gmail.com>
2022-12-26 09:35:49 +0100

    added comments

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 
 print('doing webscraping please wait')
+# makes request
 res = requests.get('https://ontheworldmap.com/all/cities/')
 res = BeautifulSoup(res.text, 'html.parser')
 
@@ -12,8 +13,10 @@ res = res.find('div',attrs={'class':'col-3'})
 cities = []
 populations = []
 for i in res.find_all('a', href=True):
+    # grabs all cities
     cities.append(f"https:{i['href']}")
 for i in cities:
+    # grabs population for every city
     res = requests.get(i)
     res = BeautifulSoup(res.text, 'html.parser')
     res = res.find('div',attrs={'class':'map-desc'}).text.split('\n')
@@ -30,6 +33,7 @@ for i in cities:
 print("done")
 populations = [int(i) for i in populations]
 
+# creates categories
 zero_to_thousand = [i for i in populations if i<1_000]
 thousand_to_ten_thousand = [i for i in populations if i>1_000 and i<10_000]
 ten_thousand_to_hundred_thousand = [i for i in populations if i>10_000 and i<100_000]
@@ -38,6 +42,7 @@ mil_to_ten_mil = [i for i in populations if i>1_000_000 and i<10_000_000]
 ten_mil_to_hundred_mil = [i for i in populations if i>10_000_000 and i<100_000_000]
 
 
+# creates chart
 data = {'under thousand':len(zero_to_thousand), 'thousand to 10 thousand':len(thousand_to_ten_thousand), '10 thousand to 100 thousand':len(ten_thousand_to_hundred_thousand), '100 thousand to mil':len(hundred_thousand_to_mil), 'mil to ten mil':len(mil_to_ten_mil), 'ten mil to 100 mil':len(ten_mil_to_hundred_mil)}
 part = list(data.keys())
 population_data = list(data.values())
